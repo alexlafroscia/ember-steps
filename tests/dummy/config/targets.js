da9f3a43c193ff7fe9db2c6d422b857b96347c09
Alex LaFroscia <alexlafroscia@nestlabs.com>
2017-05-03 12:42:53 -0700

    Run `ember init`

--- tests/dummy/config/targets.js
+++ tests/dummy/config/targets.js
@@ -1,10 +1,10 @@
- /* eslint-env node */
+/* eslint-env node */
 
- module.exports = {
-   browsers: [
-     'ie 9',
-     'last 1 Chrome versions',
-     'last 1 Firefox versions',
-     'last 1 Safari versions'
-   ]
- };
+module.exports = {
+  browsers: [
+    'ie 9',
+    'last 1 Chrome versions',
+    'last 1 Firefox versions',
+    'last 1 Safari versions'
+  ]
+};
