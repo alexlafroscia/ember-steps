c477d68982a765bbc9ef5973cc47725294daaa2a
Alex LaFroscia <alex@lafroscia.com>
2018-03-27 16:31:13 -0700

    Actually make the `assert.verify` helper work

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -11,33 +11,47 @@ import td from 'testdouble';
 
 QUnit.extend(QUnit.assert, {
   verify() {
-    this.pushResult({
-      result: true,
-      message: 'Stub passed verification'
-    });
+    try {
+      td.verify(...arguments);
+
+      this.pushResult({
+        result: true,
+        message: 'Stub passed verification'
+      });
+    } catch ({ message }) {
+      this.pushResult({
+        result: false,
+        message
+      });
+    }
   },
 
   wasCalled(fxn, expected = 1) {
-    const meta = td.explain(fxn);
-    const actual = meta.callCount;
+    const { callCount: actual } = td.explain(fxn);
+    const result = actual === expected;
 
     this.pushResult({
-      result: actual === expected,
+      result,
       actual,
       expected,
-      message: `Should have been called ${expected} times, but was called ${actual}`
+      message: result
+        ? 'Stub was called the expected number of times'
+        : `Should have been called ${expected} times, but was called ${actual}`
     });
   },
 
   wasNotCalled(fxn) {
     const meta = td.explain(fxn);
     const actual = meta.callCount;
+    const result = actual === 0;
 
     this.pushResult({
-      result: actual === 0,
+      result,
       actual,
       expected: 0,
-      message: 'Should not have been called, but it was'
+      message: result
+        ? 'Stub was not called'
+        : 'Should not have been called, but it was'
     });
   }
 });
