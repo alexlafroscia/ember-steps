5d89d615e38b46c5011ee54139a5a623b8d73eca
Jason Mitchell <jason.mitchell.w@gmail.com>
2019-03-15 13:12:03 -0700

    chore: fixing fastbooting when running via `ember s`

--- tests/dummy/config/environment.js
+++ tests/dummy/config/environment.js
@@ -21,6 +21,9 @@ module.exports = function(environment) {
       // Here you can pass flags/options to your application instance
       // when it is created
     },
+    fastboot: {
+      hostWhitelist: [/^localhost:\d+$/]
+    },
     metricsAdapters: [
       {
         name: 'GoogleAnalytics',
