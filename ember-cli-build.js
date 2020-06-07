6026b2fc8a55ea9c48043d552a45705ccb0997bf
Alex LaFroscia <alex@lafroscia.com>
2020-06-07 18:07:00 -0400

    chore: run tests with Embroider
    Followed the processes used by `ember-bootstrap` to enable testing with Embroider
    
    https://github.com/kaliber5/ember-bootstrap/commit/6acd1ce53efc601587118c653e54be169dc0a4e6

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
+const { maybeEmbroider } = require('@embroider/test-setup');
 
 module.exports = function (defaults) {
   const app = new EmberAddon(defaults, {
@@ -19,5 +20,5 @@ module.exports = function (defaults) {
     },
   });
 
-  return app.toTree();
+  return maybeEmbroider(app);
 };
