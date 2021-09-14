c62f6f91973bdea8df87ea8b969fa2a9fbe8fa24
Alex LaFroscia <alex@lafroscia.com>
2021-09-14 11:30:40 -0400

    chore(deps-dev): upgrade to `ember-qunit@5`

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -3,6 +3,7 @@ import config from '../config/environment';
 import { setApplication } from '@ember/test-helpers';
 import { start } from 'ember-qunit';
 
+import './assertions/dom';
 import './assertions/verify';
 import './assertions/wait-for';
 
